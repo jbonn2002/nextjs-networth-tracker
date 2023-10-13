f1f0c35251db95c5a8c7a8cd4b8fc5afafdc194f
Jordy <jbonn2002@gmail.com>
2023-10-13 15:21:57 -0700

    added grid to header and footer components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = async () => {
   const session = await getAuthSession();
 
   return (
-    <div className="fixed top-0 inset-x-0 border-b  text-white bg-bgGray border-zinc-300 z-[10] py-2">
+    <div className="fixed top-0 inset-x-0 border-b  text-white bg-bgGray bg-[url('/grid.svg')] border-zinc-300 z-[10] py-2">
       <div className="container max-w-7xl h-full mx-auto flex items-center gap-2">
         <Link href="/" className="flex gap-2 items-center">
           <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
